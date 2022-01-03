bf5a9cd3134ffe933a692927a5e3fcd7c734cb09
Latrell <lll4xy@virginia.edu>
2022-01-03 15:48:15 -0500

    updating the cache

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react'
-import { gql, useQuery } from '@apollo/client'
-import Persons from './Persons'
+import React, { useState } from 'react'
+import { useQuery } from '@apollo/client'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import { ALL_PERSONS } from './queries'
 
-const ALL_PERSONS = gql`
-query {
-  allPersons {
-    name
-    phone
-    id
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null
   }
+  return (
+    <div style={{color: 'red'}}>
+    {errorMessage}
+    </div>
+  )
 }
-`
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
-    <Persons persons = {result.data.allPersons} />
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons = {result.data.allPersons} />
+      <PersonForm setError={notify}/>
+    </div>
   )
 }
 
